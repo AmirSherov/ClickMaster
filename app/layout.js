47684a1a4f0644be947c671af6f41014e96b02cf
AmirSherov <sher4mir@gmail.com>
2024-12-01 21:42:41 +0500

    start up

--- app/layout.js
+++ app/layout.js
@@ -2,10 +2,15 @@ export const metadata = {
   title: "Click Master",
   description: "Game of Clicking",
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.webp" sizes="any" />
+        <link rel="icon" href="/logo.webp" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body>
         {children}
       </body>
