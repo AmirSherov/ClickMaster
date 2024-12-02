8c92f93340df1d89a20353a6895a6150ac8a7448
AmirSherov <sher4mir@gmail.com>
2024-12-02 14:30:37 +0500

    small test

--- app/page.js
+++ app/page.js
@@ -1,8 +1,21 @@
-
+'use client'
+import { useState, useEffect } from 'react';
 export default function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    const mobile = /iphone|ipod|android|webos|blackberry|iemobile|opera mini/i.test(userAgent);
+    setIsMobile(mobile);
+  }, []);
+
   return (
    <>
-   
+   <h1>
+    hello world
+   </h1>
+   <h2>
+   {isMobile ? 'Пользователь зашел с телефона' : 'Пользователь зашел с компьютера'}
+   </h2>
    </>
   );
 }
