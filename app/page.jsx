672fff594887ad4a7700f119943adb8151381ed8
AmirSherov <sher4mir@gmail.com>
2024-12-05 21:33:08 +0500

    small

--- app/page.jsx
+++ app/page.jsx
@@ -31,14 +31,14 @@ function Home() {
     dispatch({ type: 'SET_VIBRATION', payload: userData.vibration });
     dispatch({ type: 'SET_ID', payload: userData.id });
   }
-  // useEffect(() => {
-  //   const userAgent = navigator.userAgent.toLowerCase();
-  //   const mobile = /iphone|ipod|android|webos|blackberry|iemobile|opera mini/i.test(userAgent);
-  //   setIsMobile(mobile); 
-  //   if (!mobile) {
-  //     router.push('/404');
-  //   }
-  // }, [router]);
+  useEffect(() => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    const mobile = /iphone|ipod|android|webos|blackberry|iemobile|opera mini/i.test(userAgent);
+    setIsMobile(mobile); 
+    if (!mobile) {
+      router.push('/404');
+    }
+  }, [router]);
 
   return (
     <div className="landing-page-container">
