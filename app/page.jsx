b1c4f25b72638952bbc05b0da899abb200a34e95
AmirSherov <sher4mir@gmail.com>
2024-12-05 13:48:01 +0500

    small changes

--- app/page.jsx
+++ app/page.jsx
@@ -30,14 +30,14 @@ import { useGlobalContext } from './GlobalState';
     dispatch({type: 'SET_VIBRATION', payload: userData.vibration});
     dispatch({type: 'SET_ID', payload: userData.id});
   }
-  useEffect(() => {
-    const userAgent = navigator.userAgent.toLowerCase();
-    const mobile = /iphone|ipod|android|webos|blackberry|iemobile|opera mini/i.test(userAgent);
-    setIsMobile(mobile); 
-    if (!mobile) {
-      router.push('/404');
-    }
-  }, [router]);
+    // useEffect(() => {
+    //   const userAgent = navigator.userAgent.toLowerCase();
+    //   const mobile = /iphone|ipod|android|webos|blackberry|iemobile|opera mini/i.test(userAgent);
+    //   setIsMobile(mobile); 
+    //   if (!mobile) {
+    //     router.push('/404');
+    //   }
+    // }, [router]);
 
   return (
     <div className="landing-page-container">
